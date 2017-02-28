3f8bc7d4d9f9dba4c573cef216bdb38d66df379e
Victor Presumido <victor.presumido@gmail.com>
2017-02-27 21:33:13 -0300

    Add clock view

--- I'm here/FirstViewController.swift
+++ I'm here/FirstViewController.swift
@@ -11,17 +11,23 @@ import UIKit
 class FirstViewController: UIViewController{
   
   let clock = Clock()
-  
+  var timer: Timer?
   @IBOutlet weak var timeLabel: UILabel!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    let formatter = DateFormatter()
-    formatter.timeStyle = .short
-    let formattedTime = formatter.string(from: clock.currentTime as Date)
-    timeLabel.text = "\(formattedTime)"
-    
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(FirstViewController.updateTimeLabel), userInfo: nil, repeats: true)
    }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    updateTimeLabel()
+  }
+  
+  deinit {
+    timer?.invalidate()
+  }
   
 
   override func didReceiveMemoryWarning() {
@@ -29,5 +35,25 @@ class FirstViewController: UIViewController{
         // Dispose of any resources that can be recreated.
   }
   
+  func updateTimeLabel(){
+    let formatter = DateFormatter()
+    formatter.timeStyle = .short
+    let time = formatter.string(from: clock.currentTime as Date)
+    timeLabel.text = "\(time)"
+  }
+  
+  func blinkColon() -> String{
+    var formatter = DateFormatter()
+    formatter.timeStyle = .short
+    var time = formatter.string(from: clock.currentTime as Date)
+
+    formatter.dateFormat = "ss"
+    let seconds = Int(formatter.string(from: clock.currentTime as Date))
+    if(seconds! % 2 == 0){
+      
+     time = time.replacingOccurrences(of: ":", with: " ")
+    }
+    return time
+  }
 }
 
---
+++ I'm here/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  I'm here
+//
+//  Created by Victor Presumido on 2/27/17.
+//  Copyright © 2017 Victor Presumido. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Settings"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
