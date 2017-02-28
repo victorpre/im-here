814163cd21f947261d712c77c70c07e5282c1ad5
Victor Presumido <victor.presumido@gmail.com>
2017-02-28 16:36:35 -0300

    Add table View and refactor

--- I'm here/FirstViewController.swift
+++ I'm here/FirstViewController.swift
@@ -8,52 +8,82 @@
 
 import UIKit
 
-class FirstViewController: UIViewController{
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
   
   let clock = Clock()
   var timer: Timer?
+  let shifts = Shifts()
+  var myShifts : [Shift] = []
+  
   @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
   
-  
+  //  Overrides
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableView.dataSource = self
+    tableView.delegate = self
     timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(FirstViewController.updateTimeLabel), userInfo: nil, repeats: true)
    }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     updateTimeLabel()
+    print(myShifts.count)
+    shifts.drop()
   }
   
+  //Timer
   deinit {
     timer?.invalidate()
   }
   
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-  }
-  
   func updateTimeLabel(){
-    let formatter = DateFormatter()
-    formatter.timeStyle = .short
-    let time = formatter.string(from: clock.currentTime as Date)
-    timeLabel.text = "\(time)"
+    timeLabel.text = "\(blinkColon())"
   }
   
   func blinkColon() -> String{
-    var formatter = DateFormatter()
+    let formatter = DateFormatter()
     formatter.timeStyle = .short
     var time = formatter.string(from: clock.currentTime as Date)
 
     formatter.dateFormat = "ss"
     let seconds = Int(formatter.string(from: clock.currentTime as Date))
-    if(seconds! % 2 == 0){
+    if(seconds! % 2 != 0){
       
      time = time.replacingOccurrences(of: ":", with: " ")
     }
     return time
   }
+  
+  @IBAction func punch(_ sender: Any) {
+    shifts.add(time: clock.currentTime)
+    updateTableView()
+  }
+  
+  func updateTableView() {
+    myShifts = shifts.all()
+    tableView.reloadData()
+    
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return myShifts.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    let shift = myShifts[indexPath.row]
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm 'at' dd/MM/yyyy"
+    var time = formatter.string(from: shift.beginTime as! Date)
+    cell.textLabel?.text = "Started at \(time)"
+    
+    return cell
+  }
 }
+
+
+
+
 
