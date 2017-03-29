568d17e1ad587da437262cc96fee75604a2f8d04
Victor Presumido <victor.presumido@gmail.com>
2017-03-28 23:04:56 -0300

    add reports view

---
+++ I'm here/ReportViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReportViewController.swift
+//  Im here
+//
+//  Created by Victor Presumido on 3/28/17.
+//  Copyright © 2017 Victor Presumido. All rights reserved.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Estatísticas"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
