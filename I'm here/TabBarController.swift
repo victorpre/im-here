9618a28ddd222c37061b8b6e5a7c55c64486d74a
Victor Presumido <victor.presumido@gmail.com>
2017-03-31 00:02:18 -0300

    change unselected tab color

--- I'm here/TabBarController.swift
+++ I'm here/TabBarController.swift
@@ -14,7 +14,6 @@ class TabBarController: SwipeableTabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         selectedViewController = viewControllers?[0]
-        tabBar.unselectedItemTintColor = UIColor.white.withAlphaComponent(0.6)
         setSwipeAnimation(type: SwipeAnimationType.push)
     }
 }
